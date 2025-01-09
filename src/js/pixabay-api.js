de839db29935d82670325f2e803201eaeeca0dd2
Dayout <fearess7@gmail.com>
2025-01-09 22:55:41 +0200

    Added load more button

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,20 +1,31 @@
 import iziToast from 'izitoast';
+import axios from 'axios';
 
-const fetchApiData = queryValue => {
+const API_KEY = '48010442-005cbb84b5a65166ca3b031bb';
+const API_URL = 'https://pixabay.com/api/';
+
+axios.defaults.baseURL = API_URL;
+
+const fetchApiData = (queryValue, page, perPage) => {
   const searchParams = new URLSearchParams({
     key: '48134597-284d567e7eafb9cda31965081',
     q: queryValue.trim(),
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
+    page: page,
+    per_page: perPage,
   });
 
-  return fetch(`https://pixabay.com/api/?${searchParams.toString()}`)
+  return axios.get(`https://pixabay.com/api/?${searchParams}`)
     .then(response => {
-      if (!response.ok) {
+      if (response.status != 200) {
+        console.log(response);
         throw new Error(response.status);
       }
-      return response.json();
+      console.log(typeof response);
+      console.log(response);
+      return response.data;
     })
     .then(data => {
       if (!data.total) {
@@ -26,7 +37,10 @@ const fetchApiData = queryValue => {
         return [];
       }
 
-      return data.hits;
+      return {
+        images: data.hits,
+        total: data.totalHits,
+      };
     })
     .catch(error => {
       console.log(error);
--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import SimpleLightbox from 'simplelightbox';
 import fetchApiData from './js/pixabay-api';
 import generateGalleryMarkup from './js/render-functions';
 
-const formElement = document.querySelector('.js-form'),
+let page = 1, totalHits = 0, lastQuery = '';
+
+const perPage = 10,
+  loadMoreGap = 44,
+  formElement = document.querySelector('.js-form'),
   loaderElement = document.querySelector('.js-loader'),
+  loadMoreBtn = document.querySelector('.load-more'),
   galleryContainer = document.querySelector('.js-gallery'),
   simpleLightboxInstance = new SimpleLightbox('.js-gallery a.gallery-link', {
     captionDelay: 250,
@@ -12,16 +17,51 @@ const formElement = document.querySelector('.js-form'),
   submitHandler = event => {
     event.preventDefault();
     loaderElement.style.display = 'flex';
+    var query = event.target.elements['user-query'].value;
+    lastQuery = query
 
-    fetchApiData(event.target.elements['user-query'].value)
-      .then(images => {
+    fetchApiData(query, page, perPage)
+      .then(response => {
+        let images = response.images;
         galleryContainer.innerHTML = generateGalleryMarkup(images);
         simpleLightboxInstance.refresh();
+        if (response.total > perPage) {
+          loadMoreBtn.style.display = 'block';
+        }
       })
       .finally(() => {
         formElement.reset();
         loaderElement.style.display = 'none';
       });
-  };
+  },
+  loadMoreHandler = async () => {
+    page += 1;
+    fetchApiData(lastQuery, page, perPage)
+      .then(response => {
+        let images = response.images;
+        galleryContainer.innerHTML += generateGalleryMarkup(images);
+        simpleLightboxInstance.refresh();
+        let totalShownElements = page * perPage;
+        if (totalShownElements >= response.total) {
+          loadMoreBtn.style.display = 'none';
+        }
+      })
+      .finally(() => {
+        formElement.reset();
+        loaderElement.style.display = 'none';
+        setTimeout(() => {
+          const galleryCards = document.querySelectorAll('.gallery-card');
+          const lastGalleryCard = galleryCards[galleryCards.length - 1];
+          const height = lastGalleryCard.offsetHeight;
+
+          window.scrollBy({
+            top: height * 2 - loadMoreGap,
+            behavior: 'smooth',
+          });
+        }, 500);
+      });
+  }
 
 formElement.addEventListener('submit', submitHandler);
+
+loadMoreBtn.addEventListener('click', loadMoreHandler);
